31558e29d725c3b02de931b82b757e7ca021a214
Muriel Mauch <murielmauch@gmail.com>
2025-05-15 21:40:20 +0200

    Improving store. Adding models

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
+    # Leave as empty string for base url
     path("", views.store, name="store"),
     path("cart/", views.cart, name="cart"),
     path("checkout/", views.checkout, name="checkout"),
-}
+]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,15 +1,52 @@
 from django.shortcuts import render
+from .models import *
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, "store/store.html", context)
 
 
 def cart(request):
-    context = {}
+    # the store will allow purchases whether the visitor is authenticated or not,
+    # but we have to handle them differently.
+    if request.user.is_authenticated:
+        # get the customer
+        customer = request.user.customer
+        # get the incomplete order, or create a new one
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        # get the order items
+        # we can query child objects by setting the parent value (order)
+        # and the child in lowercase (orderitem)
+        items = order.orderitem_set.all()
+    else:
+        items = []  # we'll handle this later
+        order = {
+            "get_cart_total": 0,
+            "get_cart_items": 0
+        }
+    context = {"items": items, "order": order}
     return render(request, "store/cart.html", context)
 
 
 def checkout(request):
-    context = {}
+    # the store will allow purchases whether the visitor is authenticated or not,
+    # but we have to handle them differently.
+    if request.user.is_authenticated:
+        # get the customer
+        customer = request.user.customer
+        # get the incomplete order, or create a new one
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        # get the order items
+        # we can query child objects by setting the parent value (order)
+        # and the child in lowercase (orderitem)
+        items = order.orderitem_set.all()
+    else:
+        items = []  # we'll handle this later
+        order = {  # we need to set this to prevent errors when user is not logged in
+            "get_cart_total": 0,
+            "get_cart_items": 0,
+        }
+    context = {"items": items, "order": order}
+
     return render(request, "store/checkout.html", context)
